1b1c6ce56fa1f5d28fe9f5b0e4734119e80121fb
tomrcc <tom@cloudcannon.com>
2024-08-23 11:03:05 +1200

    Add rehype plugin that adds rosey tags to body content markdown text elements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import bookshop from '@bookshop/astro-bookshop';
-import tailwind from '@astrojs/tailwind';
-import alpine from '@astrojs/alpinejs';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import bookshop from "@bookshop/astro-bookshop";
+import tailwind from "@astrojs/tailwind";
+import alpine from "@astrojs/alpinejs";
+import { autoAddRoseyTags } from "./src/autoAddRoseyTags";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tiny-jackal.cloudvent.net/',
+  site: "https://tiny-jackal.cloudvent.net/",
   integrations: [react(), tailwind(), bookshop(), alpine()],
+  markdown: {
+    rehypePlugins: [autoAddRoseyTags],
+  },
 });
